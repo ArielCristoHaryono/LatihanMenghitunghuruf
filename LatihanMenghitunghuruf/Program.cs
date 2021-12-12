179535a98998eb7cbd9ac3930e4465c46ce0188a
ArielCristoHaryono <aharyono01@student.ciputra.ac.id>
2021-12-12 13:13:31 +0700

    Add project files.

---
+++ LatihanMenghitunghuruf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LatihanMenghitunghuruf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("INPUT KALIMAT: ");
+            var kalimat = Console.ReadLine();
+
+            Console.Write("INPUT KATA: ");
+            var kata = Console.ReadLine();
+            while (kata.Length < 4)
+            {
+                Console.WriteLine("huruf pada kata kurang panjang");
+                Console.Write("INPUT KATA : ");
+                kata = Console.ReadLine();
+            }
+
+        }
+    }
+}
